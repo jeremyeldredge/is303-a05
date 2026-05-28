8ae12133307578a287fd9ee8e4df30e29be5a2a6
Jeremy <jeldred1@byu.edu>
2026-05-28 10:06:26 -0600

    finished work

--- playlist.py
+++ playlist.py
@@ -21,13 +21,18 @@ Outputs:
 """
 
 class Song:
-    def __init__(self,title,artist,duration):
+    def __init__(self,title,artist,seconds):
         self.title = title
         self.artist = artist
-        self.duration = duration
+        self.seconds = seconds
 
     def __str__(self):
         return f"{self.title} by {self.artist}"
+    
+    def long_song(self):
+        return self.seconds > 300
+    
+
     
 class Playlist:
     def __init__(self,name):
@@ -35,5 +40,51 @@ class Playlist:
         self.songlist = []
 
     def __str__(self):
-        return f"{self.name} playlist with {len(self.songlist)} songs"
+        return f"Your playlist titled '{self.name}' has {len(self.songlist)} songs. Total duration: {format_duration(self.total_duration())} \n{self.convert_playlist_to_string()} \nLongest song: {self.longest_song()}"
     
+
+    def total_duration(self):
+        duration = 0
+        for song in self.songlist:
+            duration += song.seconds
+        return duration
+    
+    def longest_song(self):
+        longest = self.songlist[0]
+        for song in self.songlist:
+            if song.seconds > longest.seconds:
+                longest = song
+        return longest
+
+    def shuffle_playlist(self):
+        import random
+        random.shuffle(self.songlist)
+        return self.songlist
+    
+    def convert_playlist_to_string(self):
+        playlist_str = ""
+        for song in self.songlist:
+            playlist_str += f"{song.title} by {song.artist} ({format_duration(song.seconds)})\n"
+        return playlist_str
+    
+
+def format_duration(seconds):
+    return f"{seconds // 60}:{seconds % 60:02d}"
+
+ # --- Main Flow ---
+
+playlist_1 = Playlist("Oldies but Goodies")
+
+# song durations are input as total seconds, output formats into minutes and seconds
+song_1 = Song("Dance The Night Away", "Van Halen", 188)
+song_2 = Song("Take The Money And Run", "Steve Miller Band", 125)
+song_3 = Song("(Dont Fear) The Reaper", "Blue Oyster Cult", 308)
+song_4 = Song("Take It Easy","Eagles",211)
+
+playlist_1.songlist.append(song_1)
+playlist_1.songlist.append(song_2)
+playlist_1.songlist.append(song_3)
+playlist_1.songlist.append(song_4)
+
+playlist_1.shuffle_playlist()
+print(playlist_1)
